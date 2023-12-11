ce8e89b8fa4e4cb4d3e21d1fda8847dcb8550bbb
Kyle <kgladfelder@gmail.com>
2023-12-11 11:54:42 -0600

    feat: india trip images added and site generated

---
+++ src/lib/imageHolder.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	export let image: string;
+	export let desc: string;
+</script>
+
+<main>
+	<enhanced:img alt={desc} src={image} />
+    <h5>{desc}</h5>
+</main>
+
+<style>
+    @font-face {
+        font-family: 'Gelasio';
+        font-style: normal;
+        font-weight: 400;
+        src: local('Gelasio Regular'), local('Gelasio-Regular'), url(https://fonts.gstatic.com/s/gelasio/v1/cIf9MaFfvUQxTTqS9C6hYQ.woff2) format('woff2');
+        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+    }
+	img {
+		width: 45vw;
+		height: auto;
+		margin-left: auto;
+		margin-right: auto;
+		display: block;
+        padding-top: 1em;
+	}
+
+    @media (max-width: 800px) {
+		img {
+			width: 90vw;
+		}
+	}
+    h5 {
+        margin-left: 2em;
+        font-family: Gelasio;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,242 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import ImageHolder from '$lib/imageHolder.svelte';
+	import img0938 from '$lib/assets/IMG_0938.jpg?enhanced';
+	import img0953 from '$lib/assets/IMG_0953.jpg?enhanced';
+	import img0972 from '$lib/assets/IMG_0972.jpg?enhanced';
+	import img0973 from '$lib/assets/IMG_0973.jpg?enhanced';
+	import img0977 from '$lib/assets/IMG_0977.jpg?enhanced';
+	import img0993 from '$lib/assets/IMG_0993.jpg?enhanced';
+	import img0998 from '$lib/assets/IMG_0998.jpg?enhanced';
+	import img1005 from '$lib/assets/IMG_1005.jpg?enhanced';
+	import img1017 from '$lib/assets/IMG_1017.jpg?enhanced';
+	import img1021 from '$lib/assets/IMG_1021.jpg?enhanced';
+	import img1024 from '$lib/assets/IMG_1024.jpg?enhanced';
+	import img1039 from '$lib/assets/IMG_1039.jpg?enhanced';
+	import img1044 from '$lib/assets/IMG_1044.jpg?enhanced';
+	import img1060 from '$lib/assets/IMG_1060.jpg?enhanced';
+	import img1063 from '$lib/assets/IMG_1063.jpg?enhanced';
+	import img1067 from '$lib/assets/IMG_1067.jpg?enhanced';
+	import img1074 from '$lib/assets/IMG_1074.jpg?enhanced';
+	import img1078 from '$lib/assets/IMG_1078.jpg?enhanced';
+	import img1087 from '$lib/assets/IMG_1087.jpg?enhanced';
+	import img1098 from '$lib/assets/IMG_1098.jpg?enhanced';
+	import img1108 from '$lib/assets/IMG_1108.jpg?enhanced';
+	import img1109 from '$lib/assets/IMG_1109.jpg?enhanced';
+	import img1110 from '$lib/assets/IMG_1110.jpg?enhanced';
+	import img1115 from '$lib/assets/IMG_1115.jpg?enhanced';
+	import img1117 from '$lib/assets/IMG_1117.jpg?enhanced';
+	import img1119 from '$lib/assets/IMG_1119.jpg?enhanced';
+	import img1126 from '$lib/assets/IMG_1126.jpg?enhanced';
+	import img1129 from '$lib/assets/IMG_1129.jpg?enhanced';
+	import img1132 from '$lib/assets/IMG_1132.jpg?enhanced';
+	import img1155 from '$lib/assets/IMG_1155.jpg?enhanced';
+	import img1156 from '$lib/assets/IMG_1156.jpg?enhanced';
+	import img1157 from '$lib/assets/IMG_1157.jpg?enhanced';
+</script>
+
+<main>
+	<h1>Kyle Gladfelder's India Trip 2023</h1>
+	<div class="flex-container">
+		<div class="image-holder">
+			<ImageHolder desc="Foggy mountains near Pune India." image={img0938} />
+		</div>
+		<div class="image-holder">
+			<ImageHolder desc="Foggy Lohagad Fort near Pune India." image={img0953} />
+		</div>
+		<div class="image-holder">
+			<ImageHolder
+				desc="Fog lifting looking up at Lohagad Fort's fortifications."
+				image={img0972}
+			/>
+		</div>
+		<div class="image-holder">
+			<ImageHolder desc="More looking up at fortifications." image={img0973} />
+		</div>
+		<div class="image-holder">
+			<ImageHolder
+				desc="Me standing near one of the four gates into Lohagad Fort."
+				image={img0977}
+			/>
+		</div>
+		<div class="image-holder">
+			<ImageHolder desc="Steps heading higher into the fort." image={img0998} />
+		</div>
+		<div class="image-holder">
+			<ImageHolder
+				desc="Looking down into the valley with Visapur Fort obscured by the fog in the top right."
+				image={img0993}
+			/>
+		</div>
+		<div class="image-holder">
+			<ImageHolder
+				desc="Foggy gate entrance getting closer to the top of the fort."
+				image={img1005}
+			/>
+		</div>
+		<div class="image-holder">
+			<ImageHolder
+				desc="Looking through entryway onto the top platue of the fort."
+				image={img1017}
+			/>
+		</div>
+		<div class="image-holder">
+			<ImageHolder desc="Throne room at top of platue of Lohagad Fort." image={img1024} />
+		</div>
+		<div class="image-holder">
+			<ImageHolder desc="Throne room peaking over fortification walls." image={img1021} />
+		</div>
+		<div class="image-holder">
+			<ImageHolder desc="Looking down into the valley." image={img1039} />
+		</div>
+		<div class="image-holder">
+			<ImageHolder desc="Another angle looking down on the throne room." image={img1044} />
+		</div>
+		<div class="image-holder">
+			<ImageHolder desc="Looking out off the platue." image={img1060} />
+		</div>
+		<div class="image-holder">
+			<ImageHolder
+				desc="Looking down into the valley on the far side of the platue."
+				image={img1063}
+			/>
+		</div>
+		<div class="image-holder">
+			<ImageHolder desc="Looking back at historical fort footprint." image={img1067} />
+		</div>
+		<div class="image-holder">
+			<ImageHolder desc="Looking down towards Scorpion's Tail." image={img1074} />
+		</div>
+		<div class="image-holder">
+			<ImageHolder desc="Bird sitting in platue grass." image={img1078} />
+		</div>
+		<div class="image-holder">
+			<ImageHolder desc="Monkey sitting on fence near Scorpion's Tail path." image={img1087} />
+		</div>
+		<div class="image-holder">
+			<ImageHolder
+				desc="Directly looking down path to the tip of Scorpion's Tail."
+				image={img1098}
+			/>
+		</div>
+		<div class="image-holder">
+			<ImageHolder desc="Looking down into the valley." image={img1108} />
+		</div>
+		<div class="image-holder">
+			<ImageHolder desc="Plants in cistern at top of platue." image={img1109} />
+		</div>
+		<div class="image-holder">
+			<ImageHolder desc="Closer look at throne room." image={img1115} />
+		</div>
+		<div class="image-holder">
+			<ImageHolder desc="Colorful temple spire near Lohagad Fort and Bhaja Caves" image={img1157} />
+		</div>
+		<div class="image-holder">
+			<ImageHolder desc="Looking down into the valley." image={img1110} />
+		</div>
+		<div class="image-holder">
+			<ImageHolder
+				desc="Looking down into village around hike entrance and out to Visapur Fort."
+				image={img1117}
+			/>
+		</div>
+		<div class="image-holder">
+			<ImageHolder desc="In village looking out to second fort location." image={img1119} />
+		</div>
+		<div class="image-holder">
+			<ImageHolder desc="Bhaja Caves main entrance." image={img1126} />
+		</div>
+		<div class="image-holder">
+			<ImageHolder desc="Bhaja Caves main entrance, second angle." image={img1129} />
+		</div>
+		<div class="image-holder">
+			<ImageHolder desc="Bhaja Caves side rooms." image={img1132} />
+		</div>
+		<div class="image-holder">
+			<ImageHolder desc="Carving inside Bhaja Caves side room." image={img1155} />
+		</div>
+		<div class="image-holder">
+			<ImageHolder desc="Carving inside Bhaja Caves side room." image={img1156} />
+		</div>
+	</div>
+	<div>
+		<h3>Sites visted / seen:</h3>
+		<ul>
+			<li>
+				<a href="https://en.wikipedia.org/wiki/Lohagad">Lohagad Fort</a>
+			</li>
+			<li>
+				<a href="https://en.wikipedia.org/wiki/Visapur_Fort">Visapur Fort</a>
+			</li>
+			<li>
+				<a href="https://en.wikipedia.org/wiki/Bhaja_Caves">Bhaja Caves</a>
+			</li>
+		</ul>
+	</div>
+    <div>
+        <p>© 2023 Kyle Gladfelder</p>
+    </div>
+</main>
+
+<svelte:head>
+	<title>Kyle Gladfelder's India Trip 2023</title>
+</svelte:head>
+
+<style>
+	@font-face {
+		font-family: 'Gelasio';
+		font-style: normal;
+		font-weight: 400;
+		src:
+			local('Gelasio Regular'),
+			local('Gelasio-Regular'),
+			url(https://fonts.gstatic.com/s/gelasio/v1/cIf9MaFfvUQxTTqS9C6hYQ.woff2) format('woff2');
+		unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC,
+			U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+	}
+	.flex-container {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: space-between;
+		align-items: baseline;
+	}
+	.image-holder {
+		background-color: #d8d0c1;
+		margin-top: 1em;
+		width: 49vw;
+	}
+
+	@media (max-width: 800px) {
+		.flex-container {
+			flex-direction: column;
+		}
+		.image-holder {
+			width: 100vw;
+		}
+	}
+
+	h1 {
+		margin-left: 1em;
+		font-family: Gelasio;
+	}
+
+	/* unvisited link */
+	a:link {
+		color: #BA3F1D;
+	}
+
+	/* visited link */
+	a:visited {
+		color: #BA3F1D;
+	}
+
+	/* mouse over link */
+	a:hover {
+		color: #BA3F1D;
+	}
+
+	/* selected link */
+	a:active {
+		color: #BA3F1D;
+	}
+</style>
